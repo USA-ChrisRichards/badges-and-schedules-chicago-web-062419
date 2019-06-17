b2d19f8d600534f441372a9b2d00dffaca0b0fbf
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 23:44:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,7 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  pring attendees
+  print attendees
   batch_badge_creator(attendees).collect { |i| 
   x = batch_badge_creator[i]
   puts x
