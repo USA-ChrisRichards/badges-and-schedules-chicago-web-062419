d3a4aca233f86b7b3336d80d87e7e174b35a97f7
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 23:27:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,5 +18,5 @@ def assign_rooms(speakers)
   speakers.each_with_index do |name,index|
     speakers[name] = index
     
-  "Hello, #{name}! You'll be assigned to room #{room}!"
+  "Hello, #{name}! You'll be assigned to room #{index}!"
 end
