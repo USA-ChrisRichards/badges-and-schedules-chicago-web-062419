78ae5035577aeda21ed47229f18fde7c1d1aad63
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 23:20:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  
+def batch_badge_creator(array_of_names)
+  badge_messages = []
+  array_of_names.each do |name|
+    
+    
 end
